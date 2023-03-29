ef976937ae32a87e1ba37b2b186e704a0314c2f5
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-03-29 18:33:41 +0100

    fix: update twitter og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export const metadata: Metadata = {
     creator: "victorwill__",
     creatorId: "1243720976552144897",
     images: [
-      "https://user-images.githubusercontent.com/84178696/228617718-cf9cc287-e045-416f-8e6f-4cd9a8a191b2.png",
+      "https://user-images.githubusercontent.com/84178696/228620835-e3cc5c9b-72fc-4f54-a628-407ef7b650f5.png",
     ],
   },
   robots: {
