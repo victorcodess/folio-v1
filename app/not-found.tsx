b2ae6c2028e0f9098c722ae472a201b54a65e3f2
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-03-30 16:36:46 +0100

    feat: style 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const NotFound = () => {
   return (
-    <div>
-      <p>Uh oh! This post could not be found.</p>
+    <div className="flex h-[100vh] flex-col items-center justify-center bg-[#0E1016] text-[#e4ded7] gap-5">
+      <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] uppercase">Egbon, you don miss road oh!</p>
+      <Image
+        src="https://user-images.githubusercontent.com/84178696/228884970-66a9687c-b364-48e4-91ca-05332dadbba9.png"
+        priority
+        alt="Funny Meme"
+        width={858}
+        height={483}
+        className="w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%]"
+      />
       <div>
-        <Link href="/">Go back to Home</Link>
+        <Link href="/" className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] uppercase">
+          Head back to my{" "}
+          <span className="underline underline-offset-2">Portfolio</span>
+        </Link>
       </div>
     </div>
   );
