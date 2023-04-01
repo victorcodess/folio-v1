2048462e51db76f8628dec7cc49e3f4dd7c9a6e5
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-04-01 12:16:11 +0100

    feat: redesign about-section, edit animatons

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -8,98 +8,104 @@ import AnimatedWords2 from "../animations/AnimatedWords2";
 const About = () => {
   return (
     <section
-      className="relative z-10 w-full items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pt-16 pb-24 md:pt-20 md:pb-44 lg:pt-20 lg:pb-52"
+      className=" relative z-10 w-full items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pt-16 pb-24 md:pt-20 md:pb-44 lg:pt-20 lg:pb-52"
       id="about"
     >
-      <div className="mx-auto flex w-[90%] flex-col lg:max-w-[1200px] lg:flex-row lg:gap-20">
-        <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-medium  leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[20px] md:leading-relaxed lg:mb-16  lg:max-w-[90%] lg:text-[24px] ">
-          <AnimatedTitle
-            text={"Hello there!"}
-            className={
-              "hidden text-[40px] font-bold text-[#e4ded7] md:text-[44px] lg:text-[48px]"
-            }
-            wordSpace={"mr-[0.25em]"}
-            charSpace={"mr-[0.01em]"}
-          />
+      <div className="mx-auto flex w-[90%] flex-col lg:max-w-[1200px]">
+        <h2 className="mb-20 text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] md:text-[44px] lg:text-[80px]">
+          I MAKE BRANDS BEAUTIFUL, WEBSITES POWERFUL AND CONTENT CAPTIVATING.
+        </h2>
+        {/* <AnimatedTitle
+          text={
+            "WE MAKE BRANDS BEAUTIFUL, WEBSITES POWERFUL AND CONTENT CAPTIVATING. A PURPOSE-DRIVEN STUDIO DARING TO BE DIFFERENT."
+          }
+          className={
+            " mb-40 inline-flex  border text-[40px] font-bold text-[#e4ded7] md:text-[44px] lg:text-[72px]"
+          }
+          wordSpace={"mr-[0.25em]"}
+          charSpace={"mr-[0.01em]"}
+        /> */}
+        <div className="mx-auto flex w-[100%] flex-col lg:max-w-[1200px] lg:flex-row lg:gap-20">
+          <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-medium  leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[20px] md:leading-relaxed lg:mb-16  lg:max-w-[90%] lg:text-[24px] ">
+            <AnimatedBody
+              text={
+                "My passion lies in creating software that solves real-world problems and positively impacts people’s lives."
+              }
+            />
+            <AnimatedBody
+              text={
+                "Whether I’m designing a sleek user interface or coding a complex application, I’m always striving to create something unique and innovative. I love experimenting with new technologies and staying up-to-date with the latest trends in the tech world."
+              }
+              className={"hidden"}
+            />
+            <AnimatedBody
+              text={
+                "In addition to my work in software development, I’m also an active member of tech communities on campus, including GDSC and MLSA."
+              }
+            />
+            <AnimatedBody
+              text={
+                "Whether it’s binge-watching my favorite anime series, cheering on Manchester United, hanging out with friends, or discovering new music in my favorite genres like RnB, UK Drill, and Chill Rap, I’m always looking for ways to relax and unwind."
+              }
+            />
+            <AnimatedBody
+              text={
+                "Right now, I’m working on some exciting projects that I can’t wait to share with you. But I’m always open to new opportunities and collaborations."
+              }
+            />
+          </div>
 
-          <AnimatedBody
-            text={
-              "I’m a front-end engineer and product designer based in Lagos, Nigeria. My passion lies in creating software that solves real-world problems and positively impacts people’s lives."
-            }
-          />
-          <AnimatedBody
-            text={
-              "Whether I’m designing a sleek user interface or coding a complex application, I’m always striving to create something unique and innovative. I love experimenting with new technologies and staying up-to-date with the latest trends in the tech world."
-            }
-            className={"hidden"}
-          />
-          <AnimatedBody
-            text={
-              "In addition to my work in software development, I’m also an active member of tech communities on campus, including GDSC and MLSA. I’m always eager to collaborate with other passionate individuals to build solutions for local problems and gain new skills through hands-on workshops, events, talks, and project-building activities."
-            }
-          />
-          <AnimatedBody
-            text={
-              "I also love indulging in my hobbies when I’m not busy in VSCode or Figma. Whether it’s binge-watching my favorite anime series, cheering on Manchester United, hanging out with friends, or discovering new music in my favorite genres like RnB, UK Drill, and Chill Rap, I’m always looking for ways to relax and unwind."
-            }
-          />
-          <AnimatedBody
-            text={
-              "Right now, I’m working on some exciting projects that I can’t wait to share with you. But I’m always open to new opportunities and collaborations. If you’re a hiring manager or startup founder/CTO who’s looking for someone with both technical skills and a creative edge, then look no further. Get in touch with me on any of the platforms listed below, and let’s chat!"
-            }
-          />
-        </div>
-
-        <div className="mb-24 flex w-[100%] flex-col gap-4 text-[18px] font-normal leading-relaxed tracking-wide text-[#e4ded7]/80 sm:mb-32 md:mb-40 md:gap-6 md:text-[16px] md:leading-normal lg:mt-0 lg:mb-16 lg:max-w-[30%] lg:text-[18px]">
-          <div className="flex flex-col gap-4 md:gap-3">
-            <AnimatedTitle
-              text={"Frontend Tools"}
-              className={
-                "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
-              }
-              wordSpace={"mr-[0.25em]"}
-              charSpace={"mr-[0.01em]"}
-            />
-            <AnimatedBody
-              text={
-                "JavaScript(ES6+), React, Next.js, TypeScript, Redux, Redux Toolkit, React Testing Library, Vitetest, HTML5, Git/GitHub, NextAuth, Formik."
-              }
-            />
-          </div>
-          <div className="flex flex-col gap-3">
-            <AnimatedTitle
-              text={"UI Libraries"}
-              className={
-                "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
-              }
-              wordSpace={"mr-[0.25em]"}
-              charSpace={"mr-[0.01em]"}
-            />
-            <AnimatedBody
-              text={
-                "CSS3/SCSS/SASS, Tailwind CSS, Material UI, Framer Motion, Bootstrap, Chart.js."
-              }
-            />
-          </div>
-          <div className="flex flex-col gap-3">
-            <AnimatedTitle
-              text={"Design Tools"}
-              className={
-                "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
-              }
-              wordSpace={"mr-[0.25em]"}
-              charSpace={"mr-[0.01em]"}
-            />
-            <AnimatedBody
-              text={
-                "  Figma, FigJam, Adobe XD, ProtoPie, Adobe Photoshop, UX Research, UI Design, Prototyping."
-              }
-            />
+          <div className="mb-24 flex w-[100%] flex-col gap-4 text-[18px] font-normal leading-relaxed tracking-wide text-[#e4ded7]/80 sm:mb-32 md:mb-40 md:gap-6 md:text-[16px] md:leading-normal lg:mt-0 lg:mb-16 lg:max-w-[30%] lg:text-[18px]">
+            <div className="flex flex-col gap-4 md:gap-3">
+              <AnimatedTitle
+                text={"Frontend Tools"}
+                className={
+                  "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
+                }
+                wordSpace={"mr-[0.25em]"}
+                charSpace={"mr-[0.01em]"}
+              />
+              <AnimatedBody
+                text={
+                  "JavaScript(ES6+), React, Next.js, TypeScript, Redux, Redux Toolkit, React Testing Library, Vitetest, HTML5, Git/GitHub, NextAuth, Formik."
+                }
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <AnimatedTitle
+                text={"UI Libraries"}
+                className={
+                  "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
+                }
+                wordSpace={"mr-[0.25em]"}
+                charSpace={"mr-[0.01em]"}
+              />
+              <AnimatedBody
+                text={
+                  "CSS3/SCSS/SASS, Tailwind CSS, Material UI, Framer Motion, GSAP, Bootstrap, Chart.js."
+                }
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <AnimatedTitle
+                text={"Design Tools"}
+                className={
+                  "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
+                }
+                wordSpace={"mr-[0.25em]"}
+                charSpace={"mr-[0.01em]"}
+              />
+              <AnimatedBody
+                text={
+                  "  Figma, FigJam, Adobe XD, ProtoPie, Adobe Photoshop, UX Research, UI Design, Prototyping."
+                }
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="mt-10 md:-mt-0 lg:mt-32">
-        <SongCarousel />
+        <div className="mt-10  md:-mt-0 lg:mt-28">
+          <SongCarousel />
+        </div>
       </div>
     </section>
   );
--- app/animations/animations.tsx
+++ app/animations/animations.tsx
@@ -24,7 +24,7 @@ export const imageAnimation = {
     scale: 1,
     y: `0em`,
     transition: {
-      delay: 6,
+      delay: 6.5,
       duration: 1,
       ease: [0.2, 0.65, 0.3, 0.9],
     },
