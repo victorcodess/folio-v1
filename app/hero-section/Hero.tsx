367c8d39d7053dca8021a904177730ed42cd6554
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-06-21 08:14:21 +0100

    chore: update resume link

--- app/hero-section/Hero.tsx
+++ app/hero-section/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
       <div className="absolute top-10 flex justify-between sm:w-[90%] lg:max-w-[1440px]">
         <div>
           <Link
-            href="https://drive.google.com/file/d/1wXiRUoNtjP3jxPnuFPj9gIbuOwouUfho/view"
+            href="https://drive.google.com/file/d/16ZMMlmiHAs94WrFO7FPF35aTEthNcXHh/view"
             target="_blank"
             aria-label="View Resume"
           >
--- app/navbar/NavBar.tsx
+++ app/navbar/NavBar.tsx
@@ -20,7 +20,7 @@ const NavBar = () => {
   return (
     <nav className="fixed bottom-10 left-0 right-0 z-50 my-0  mx-auto  flex w-[306px] items-center justify-center gap-1 rounded-lg bg-[#07070a]/90 px-1 py-1 text-[#e4ded7] backdrop-blur-md sm:w-[383.3px] md:p-2 lg:w-[391.3px]">
       <Link
-        href="https://drive.google.com/file/d/1wXiRUoNtjP3jxPnuFPj9gIbuOwouUfho/view"
+        href="https://drive.google.com/file/d/16ZMMlmiHAs94WrFO7FPF35aTEthNcXHh/view"
         target="_blank"
         className="flex"
         aria-label="Open my resume"
