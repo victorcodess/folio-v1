1b7d47cc988c4c992a76f01e351947fcf60ef5c9
Victor Williams <victor.williams@stu.cu.edu.ng>
2024-06-09 16:22:40 +0100

    chore: update copyright year

--- app/footer/Footer.tsx
+++ app/footer/Footer.tsx
@@ -3,6 +3,8 @@ import { motion } from "framer-motion";
 import AnimatedBody from "../animations/AnimatedBody";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+  
   return (
     <motion.section
       className=" h-[15vh] w-full  items-center justify-center border-t-2 border-[#e4ded7]/30 bg-[#0E1016] pt-10  font-bold uppercase md:h-[20vh] md:py-16 lg:h-[10vh] lg:pt-6 lg:pb-0"
@@ -10,7 +12,7 @@ const Footer = () => {
       animate="animate"
     >
       <motion.div className="mx-auto flex w-[90%] flex-row items-center justify-between text-center text-[12px] text-[#e4ded7] sm:text-[12px] md:text-[14px] lg:max-w-[1440px] lg:text-[14px]">
-        <AnimatedBody text={"Copyright 2023"} className={"m-0 p-0"} />
+        <AnimatedBody text={`Copyright ${year}`} className={"m-0 p-0"} />
         <div className="flex flex-col sm:flex-row  sm:gap-1 md:gap-2">
           <AnimatedBody
             text={"Design & Development by"}
