b89fa3cb58077d3039193c317448e7582b22ceb7
Victor Williams <victor.williams@stu.cu.edu.ng>
2024-08-19 05:20:16 +0100

    feat: add odunsi project

--- app/work-section/projectDetails.ts
+++ app/work-section/projectDetails.ts
@@ -12,59 +12,70 @@ export type ProjectProps = {
 export const devProjects = [
   {
     id: 0,
+    name: "Odunsi",
+    description:
+      "Portfolio website for Michael Odunsi, an experienced UI/UX designer crafting unique, user-friendly products and web experiences for Web3 founders and projects.",
+    technologies: ["React", "Tailwind CSS", "Framer Motion"],
+    github: "https://github.com/victorcodess/odunsi-web3-folio",
+    demo: "https://www.odunsi.xyz/",
+    image: require(".//../../public/projects/odunsi.png"),
+    available: true,
+  },
+  {
+    id: 1,
     name: "Interlock",
     description:
       "This is a website for a Fintech Startup to showcase their innovative solutions tailored to meet the evolving needs of their clients.",
     technologies: ["React", "Tailwind CSS", "Framer Motion"],
     github: "https://github.com/victorcodess/interlock",
     demo: "https://interlock-teal.vercel.app/",
-    image: require(".//../../public/projects/interlock-flip2.png"),
+    image: require(".//../../public/projects/interlock.png"),
     available: true,
   },
   {
-    id: 1,
+    id: 2,
     name: "Synthetix",
     description:
       "Built specifically for an AI startup, this website lets them present cutting-edge AI data processing solutions tailored to their customers' needs.",
     technologies: ["React", "Next.js", "Prismic CMS"],
     github: "https://github.com/victorcodess/synthetix",
     demo: "https://synthetix-iota.vercel.app/",
-    image: require(".//../../public/projects/synthetix.png"),
+    image: require(".//../../public/projects/synthetix-flip.png"),
     available: true,
   },
   {
-    id: 2,
+    id: 3,
     name: "Propellent",
     description:
       "A website built for a software startup and small business, to showcase their services and mark their online presence.",
     technologies: ["React", "Tailwind CSS", "Framer Motion"],
     github: "https://github.com/victorcodess/propellent",
     demo: "https://propellent.vercel.app/",
-    image: require(".//../../public/projects/propellent-new-flip.png"),
+    image: require(".//../../public/projects/propellent-new.png"),
     available: true,
   },
   {
-    id: 3,
+    id: 4,
     name: "Flixify",
     description:
       "Flixify lets you seamlessly explore movies and TV series, add bookmarks, and search across all pages. It offers user authentication along with a theme switch.",
     technologies: ["Next.js", "Typescript", "Prisma"],
     github: "https://github.com/victorcodess/flixify",
     demo: "https://flixify.victorwilliams.me/",
-    image: require(".//../../public/projects/flixify-flip.png"),
+    image: require(".//../../public/projects/flixify.png"),
     available: true,
   },
-  {
-    id: 4,
-    name: "SkyWatch",
-    description:
-      "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current weather and forecast for the next 2 days in any city.",
-    technologies: ["React", "CSS", "Chart.js"],
-    github: "https://github.com/victorcodess/weather-forecast-website",
-    demo: "https://sky-watch.vercel.app/",
-    image: require(".//../../public/projects/skywatch-flip.png"),
-    available: true,
-  },
+  // {
+  //   id: 4,
+  //   name: "SkyWatch",
+  //   description:
+  //     "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current weather and forecast for the next 2 days in any city.",
+  //   technologies: ["React", "CSS", "Chart.js"],
+  //   github: "https://github.com/victorcodess/weather-forecast-website",
+  //   demo: "https://sky-watch.vercel.app/",
+  //   image: require(".//../../public/projects/skywatch-flip.png"),
+  //   available: true,
+  // },
   // {
   //   id: 4,
   //   name: "Alpaca Image Generator",
